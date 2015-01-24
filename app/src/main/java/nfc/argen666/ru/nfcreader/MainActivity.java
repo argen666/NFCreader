f2f93b51c30c0de3aef7fed7dd5458d2a230a109
argen666 <argen666@gmail.com>
2015-01-25 01:35:15 +0600

    Initial commit

---
+++ app/src/main/java/nfc/argen666/ru/nfcreader/MainActivity.java
@@ -0,0 +1,171 @@
+package nfc.argen666.ru.nfcreader;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcF;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    NfcAdapter mAdapter;
+    IntentFilter[] intentFiltersArray;
+    PendingIntent pendingIntent;
+    String[][] techListsArray;
+    Boolean mode=false;
+    String data="";
+TextView textView1;
+    ProgressBar pg;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+        pg = (ProgressBar) findViewById(R.id.progressBar);
+        // enable foreground dispatch
+         mAdapter = NfcAdapter.getDefaultAdapter(this);
+         pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");    /* Handles all MIME based dispatches.
+                                       You should specify only the ones that you need. */
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+         intentFiltersArray = new IntentFilter[] {ndef, };
+         techListsArray = new String[][] { new String[] { NfcA.class.getName() } };
+        if (savedInstanceState==null){
+        resolveIntent(getIntent());
+    }
+    else {
+
+}
+    }
+
+    void resolveIntent(Intent intent) {
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())){
+        //textView1.setText("Please Wait...");
+          //  Toast.makeText(getApplicationContext(),"Card Detected",Toast.LENGTH_SHORT).show();
+        NFCReader reader = new NFCReader();
+        reader.setPg(pg);
+        reader.execute(intent, mode);
+            try {
+                data =reader.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            textView1.setText(data);}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i(TAG, "created options");
+        return true;
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        resolveIntent(intent);
+        Log.i(TAG, "new intent");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.disableForegroundDispatch(this);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        // Операции для выбранного пункта меню
+        switch (item.getItemId())
+        {
+            case R.id.mode:{
+                if (!mode)
+                {
+                   mode =true;
+                   item.setTitle("Full mode: ON");
+                   textView1.setTextSize(20);
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT, Gravity.TOP);
+                    textView1.setLayoutParams(params);
+                    textView1.setText("");
+                }
+                else {
+                    mode=false;
+                    item.setTitle("Full mode: OFF");
+                    textView1.setTextSize(60);
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+               textView1.setLayoutParams(params);
+                    textView1.setText("");
+                }
+            }
+
+
+
+        }
+        return true;
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("uid", data);
+        super.onSaveInstanceState(outState);
+
+        Log.d(TAG, "onSaveInstanceState");
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        data = savedInstanceState.getString("uid");
+        textView1.setText(data=="" ? "Жду карту" : data);
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+}
---
+++ app/src/main/java/nfc/argen666/ru/nfcreader/NFCReader.java
@@ -0,0 +1,152 @@
+package nfc.argen666.ru.nfcreader;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Developer on 24.01.2015.
+ */
+class NFCReader extends AsyncTask<Object, Void, String> {
+    String LOG_TAG = "WWWWWWWW";
+    ProgressBar pgb;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(pgb.getContext(),"Card Detected",Toast.LENGTH_SHORT).show();
+        pgb.setVisibility(View.VISIBLE);
+        Log.d(LOG_TAG, "Begin");
+    }
+
+    public void setPg(ProgressBar pg) {
+        this.pgb = pg;
+    }
+
+    @Override
+    protected String doInBackground(Object... params) {
+        Intent intent = (Intent) params[0];
+        Boolean full = (Boolean) params[1];
+        Log.i(LOG_TAG, "resolving intent");
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        String metaInfo = "";
+        if (tag != null) {
+            Log.i(LOG_TAG, "found a tag");
+            //System.out.println(tag.getTechList().length);
+
+            metaInfo += "Supported technologies:\n";
+            for (String tech : tag.getTechList()) {
+                metaInfo += tech + "\n";
+            }
+            boolean auth = false;
+            MifareClassic mfc = MifareClassic.get(tag);
+            if (mfc == null) {
+                //textView1.setText("Unsupported card! (No Mifare/NfcA)");
+                return "Unsupported card! (No Mifare/NfcA)";
+            }
+            try {
+
+                //Enable I/O operations to the tag from this TagTechnology object.
+                mfc.connect();
+                int type = mfc.getType();
+                int sectorCount = mfc.getSectorCount();
+                String typeS = "";
+                switch (type) {
+                    case MifareClassic.TYPE_CLASSIC:
+                        typeS = "TYPE_CLASSIC";
+                        break;
+                    case MifareClassic.TYPE_PLUS:
+                        typeS = "TYPE_PLUS";
+                        break;
+                    case MifareClassic.TYPE_PRO:
+                        typeS = "TYPE_PRO";
+                        break;
+                    case MifareClassic.TYPE_UNKNOWN:
+                        typeS = "TYPE_UNKNOWN";
+                        break;
+                }
+                metaInfo += "Card type：" + typeS + "\n with" + sectorCount + " Sectors,\n "
+                        + mfc.getBlockCount() + " Blocks\nStorage Space: " + mfc.getSize() + "B\n";
+                if (full) {
+                    for (int j = 0; j < sectorCount; j++) {
+                        //Authenticate a sector with key A.
+                        auth = mfc.authenticateSectorWithKeyA(j,
+                                MifareClassic.KEY_DEFAULT);
+                        int bCount;
+                        int bIndex;
+                        if (auth) {
+                            metaInfo += "Sector " + j + ": Verified successfully\n";
+                            bCount = mfc.getBlockCountInSector(j);
+                            bIndex = mfc.sectorToBlock(j);
+                            for (int i = 0; i < bCount; i++) {
+                                byte[] data = mfc.readBlock(bIndex);
+                                metaInfo += "Block " + bIndex + " : "
+                                        + bytesToHex(data) + "\n";
+                                bIndex++;
+                            }
+                        } else {
+                            metaInfo += "Sector " + j + ": Verified failure\n";
+                        }
+                    }
+                } else {
+                    auth = mfc.authenticateSectorWithKeyA(0,
+                            MifareClassic.KEY_DEFAULT);
+                    int bCount;
+                    int bIndex;
+                    String uid = "";
+                    String fcode = "";
+                    if (auth) {
+                        metaInfo="";
+                        bCount = mfc.getBlockCountInSector(0);
+                        bIndex = mfc.sectorToBlock(0);
+                        for (int i = 0; i < 1; i++) {
+                            byte[] data = mfc.readBlock(bIndex);
+                            metaInfo += bytesToHex(data);
+                            bIndex++;
+                        }
+
+                        fcode=metaInfo.substring(4, 6);
+                        uid=metaInfo.substring(0, 4);
+                        uid = uid.substring(2, 4)+uid.substring(0, 2);
+                        metaInfo="";
+                        metaInfo += String.valueOf(Integer.parseInt(fcode,16));
+                        metaInfo += String.valueOf(Integer.parseInt(uid,16));
+                    } else {
+                        metaInfo += "Sector " + 0 + ": Verified failure\n";
+                    }
+                }
+
+                //textView1.setText(metaInfo);
+
+            } catch (Exception e) {
+                //textView1.setText("Lost connection!");
+                e.printStackTrace();
+            }
+        }
+
+        return metaInfo;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        final char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        pgb.setVisibility(View.GONE);
+    }
+}
